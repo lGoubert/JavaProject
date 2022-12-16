d28ee029eaddf5ddc7f7a290a8191fb7df37ba6e
Enzo Laurence <enzo.laurence9741@gmail.com>
2022-12-16 10:58:37 +0100

    Correction des noms

--- Image/manage-country.py
+++ Image/manage-country.py
@@ -2,6 +2,9 @@
 import os
 import base64
 
+
+
+#Main
 def main():
     print("Start")
 
@@ -13,20 +16,40 @@ def main():
     
     #Loop through the files
     for f in files:
+
         #If the file is a png file
         if f.endswith(".png"):
+
             #Get the name of the country
             country = f.split(".")[0]
+
             #Transform the image into base64 using base64.b64encode()
             flag = base64.b64encode(open(f, "rb").read()).decode("utf-8")
             
             #Create a insert statement int the table country with the following value flags wihch is the image (blob), country which is the country name and difficulty wich is at one.
-            file.write("INSERT INTO country (flag, country, difficulty) VALUES ('" + flag + "', '" + country + "', 1);")
+            file.write("INSERT INTO countries (flag, country, difficulty) VALUES ('" + flag + "', '" + country + "', 1);")
 
     #Close the file
     file.close()
 
+    #Ask the user if the script should be executed
+    if input("Do you want to execute the script? (y/n)") == "y":
+        #Execute the script
+        insert_into_database()
+
     print("End")
+
+#Def InsertIntoDatabase
+def insert_into_database():
+    #Variables
+    user = "javaprojet"
+    password = "devops"
+    database = "javaprojet"
+    host = "45.155.169.116"
+    port= "6006"
+
+    #Execute the script into the database using the variables
+    os.system("mysql -u " + user + " -p" + password + " -D " + database + " -h " + host + " -P " + port + " < countries.sql")
 
 #If Main
 if __name__ == "__main__":
--- sql/JavaProjet_Database_mysql_create.sql
+++ sql/JavaProjet_Database_mysql_create.sql
@@ -5,36 +5,36 @@ CREATE TABLE `users` (
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `country` (
+CREATE TABLE `countries` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`flags` longblob NOT NULL,
+	`flag` blob NOT NULL,
 	`country` varchar(255) NOT NULL,
 	`difficulty` INT(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `games_score` (
+CREATE TABLE `games_scores` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`id_users_one` INT NOT NULL,
-	`id_users_two` INT NOT NULL,
-	`score_users_one` INT NOT NULL,
-	`score_users_two` INT NOT NULL,
+	`id_user_one` INT NOT NULL,
+	`id_user_two` INT NOT NULL,
+	`score_user_one` INT NOT NULL,
+	`score_user_two` INT NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `messages` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`id_senders` INT NOT NULL,
+	`id_sender` INT NOT NULL,
 	`id_receiver` INT NOT NULL,
 	`message` TEXT NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-ALTER TABLE `games_score` ADD CONSTRAINT `games_score_fk0` FOREIGN KEY (`id_users_one`) REFERENCES `users`(`id`);
+ALTER TABLE `games_scores` ADD CONSTRAINT `games_scores_fk0` FOREIGN KEY (`id_user_one`) REFERENCES `users`(`id`);
 
-ALTER TABLE `games_score` ADD CONSTRAINT `games_score_fk1` FOREIGN KEY (`id_users_two`) REFERENCES `users`(`id`);
+ALTER TABLE `games_scores` ADD CONSTRAINT `games_scores_fk1` FOREIGN KEY (`id_user_two`) REFERENCES `users`(`id`);
 
-ALTER TABLE `messages` ADD CONSTRAINT `messages_fk0` FOREIGN KEY (`id_senders`) REFERENCES `users`(`id`);
+ALTER TABLE `messages` ADD CONSTRAINT `messages_fk0` FOREIGN KEY (`id_sender`) REFERENCES `users`(`id`);
 
 ALTER TABLE `messages` ADD CONSTRAINT `messages_fk1` FOREIGN KEY (`id_receiver`) REFERENCES `users`(`id`);
 
