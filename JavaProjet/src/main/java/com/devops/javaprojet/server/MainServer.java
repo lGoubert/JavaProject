273bdc1192ec67a5403c3b954c75a953554541b1
Lucas Goubert <lgoubert@groupeoptimis.fr>
2023-01-13 08:41:49 +0100

    Création mains

--- JavaProjet/src/main/java/com/devops/javaprojet/server/MainServer.java
+++ JavaProjet/src/main/java/com/devops/javaprojet/server/MainServer.java
@@ -1,4 +1,30 @@
 package com.devops.javaprojet.server;
-
+import java.net.*;
+import java.io.*;
 public class MainServer {
+    public static void main(String[] args) throws IOException {
+        int port = 1234; // Le numéro de port sur lequel le serveur écoutera
+
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Le serveur est en écoute sur le port " + port);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept(); // Attente de la connexion d'un client
+            System.out.println("Connexion établie avec " + clientSocket.getInetAddress());
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            String inputLine, outputLine;
+            while ((inputLine = in.readLine()) != null) {
+                // Traitement de la requête du client
+                outputLine = "Réponse du serveur : " + inputLine;
+                out.println(outputLine);
+                if (inputLine.equals("Bye."))
+                    break;
+            }
+
+            clientSocket.close();
+        }
+    }
 }
