1d8e7a32c879a8fd4a0acff549f2807eaa981ded
Enzo Laurence <enzo.laurence9741@gmail.com>
2023-01-13 11:50:47 +0100

    Avancement API

--- JavaProjet/src/main/java/com/devops/javaprojet/server/api/Api.java
+++ JavaProjet/src/main/java/com/devops/javaprojet/server/api/Api.java
@@ -13,6 +13,19 @@ public class Api {
     }
 
     public void CreateUser(String username, String password) throws SQLException, NoSuchAlgorithmException {
-        databaseDAO.InsertNewUser(username, password);
+        databaseDAO.InsertNewUser(username.toLowerCase(), password);
     }
+
+    public String Login(String username, String password) throws SQLException {
+        switch (databaseDAO.GetUserInfo(username.toLowerCase(), password)){
+            case 0:
+                return "L'ensemble username/password est incorrect";
+            case 1:
+                return "Connexion valide bienvenue ! " + username;
+            default:
+                return "Erreur lors de la connexion";
+        }
+    }
+
+    public void GetGameResponse("")
 }
